6e5f0efc767880656970a278032f63ce1b31cf29
Henrique Eduardo <heduardocr7@gmail.com>
2025-05-30 20:28:42 -0300

    feat: install react query and configure in the global application layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 const roboto = Roboto({
   variable: "--font-roboto",
@@ -18,12 +22,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${roboto.variable} $ antialiased`}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          {children}
-        </ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
