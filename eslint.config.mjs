58fe3b834cbc8c9b1fa634cca9bc3d0414080b67
Henrique Eduardo <heduardocr7@gmail.com>
2025-05-30 13:57:48 -0300

    feat: start project and apply default settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+    },
+  }),
 ];
 
 export default eslintConfig;
