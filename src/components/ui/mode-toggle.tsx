3fd7f126b745db24e293d6aa4f03a1f0efeef88b
Henrique Eduardo <heduardocr7@gmail.com>
2025-05-30 14:18:58 -0300

    feat: add dark mode using next-themes

---
+++ src/components/ui/mode-toggle.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+
+export function ModeToggle() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button
+      onClick={toggleTheme}
+      aria-label={`Switch to ${theme === "light" ? "dark" : "light"} theme`}
+      className="border-brand-button"
+    >
+      {theme === "light" ? (
+        <MoonIcon className="h-[1.5rem] w-[1.5rem]" />
+      ) : (
+        <SunIcon className="h-[1.5rem] w-[1.5rem]" />
+      )}
+    </Button>
+  );
+}
