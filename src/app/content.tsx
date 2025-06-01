9df19bf771945ffacaf96af81b227f2e0e7a99b3
Henrique Eduardo <heduardocr7@gmail.com>
2025-06-01 19:25:46 -0300

    remove: delete file content

--- src/app/content.tsx
+++
@@ -1,9 +0,0 @@
-import { ContactsList } from "./_components/contacts-list";
-
-export function Content() {
-  return (
-    <main className="flex flex-col w-full min-h-screen bg-background gap-8">
-      <ContactsList />
-    </main>
-  );
-}
